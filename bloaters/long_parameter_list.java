4f3dc7a4f805eb3b559fa85c6f6ec4bc96774952
randyRivera0 <rariberm@espol.edu.ec>
2024-08-12 19:46:47 -0500

    modified:   bloaters/long_parameter_list.java

--- bloaters/long_parameter_list.java
+++ bloaters/long_parameter_list.java
@@ -20,67 +20,6 @@ public class long_parameter_list {
             this.bibliografia = bibliografia;
         }
 
-        // Getters y setters (opcional, pero útil para acceso posterior)
-        public String getTitulo() {
-            return titulo;
-        }
-
-        public void setTitulo(String titulo) {
-            this.titulo = titulo;
-        }
-
-        public String getAutor() {
-            return autor;
-        }
-
-        public void setAutor(String autor) {
-            this.autor = autor;
-        }
-
-        public String getEditorialCodigo() {
-            return editorialCodigo;
-        }
-
-        public void setEditorialCodigo(String editorialCodigo) {
-            this.editorialCodigo = editorialCodigo;
-        }
-
-        public String getIndice() {
-            return indice;
-        }
-
-        public void setIndice(String indice) {
-            this.indice = indice;
-        }
-
-        public String getContenido() {
-            return contenido;
-        }
-
-        public void setContenido(String contenido) {
-            this.contenido = contenido;
-        }
-
-        public String getBibliografia() {
-            return bibliografia;
-        }
-
-        public void setBibliografia(String bibliografia) {
-            this.bibliografia = bibliografia;
-        }
-
-        @Override
-        public String toString() {
-            return "GenerateReport{" +
-                   "titulo='" + titulo + '\'' +
-                   ", autor='" + autor + '\'' +
-                   ", editorialCodigo='" + editorialCodigo + '\'' +
-                   ", indice='" + indice + '\'' +
-                   ", contenido='" + contenido + '\'' +
-                   ", bibliografia='" + bibliografia + '\'' +
-                   '}';
-        }
-
         // Método principal para probar la clase
         public static void main(String[] args) {
             GenerateReport report = new GenerateReport(
